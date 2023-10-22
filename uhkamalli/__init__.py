2f64e7f582597d9b084c3f14ec3980f62b0dd314
Stefan Hagen <stefan@hagen.link>
2023-10-22 14:27:52 +0200

    Bumped impl version for release 2023.10.22

--- uhkamalli/__init__.py
+++ uhkamalli/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2023.10.22+parent.dirty'
-# [[[end]]] (checksum: 2ec7336ee3433f0f339b27afb5f776df)
+__version__ = '2023.10.22+parent.8d93fb85'
+# [[[end]]] (checksum: 0266f11fb6403df290b631f426be17d5)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
